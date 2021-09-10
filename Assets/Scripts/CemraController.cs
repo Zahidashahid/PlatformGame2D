5773c258d3f1cf030cfc4050ed0496bbb4238840
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-09-10 23:55:03 +0500

    Player movement

---
+++ Assets/Scripts/CemraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CemraController : MonoBehaviour
+{
+    public Transform player;
+    public GameObject camera;
+   
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x , player.position.y, transform.position.z);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public GameObject player;
+    void Awake()
+    {
+        player = new GameObject();
+    }
     private void Update()
     { 
         // Move Player back
         if(Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
         }
         // Move Player Forward
         if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
         }
         // Jump Player 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
         }
-        if(Input.GetKey(KeyCode.S))
+        /*if(Input.GetKey(KeyCode.S))
         {
             rb.velocity = new Vector2(+5, 0);
-        }
+        }*/
     }
 }
