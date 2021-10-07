2b618662acda8e627add6888520cdd537e7fd132
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-10-07 14:51:03 +0500

    Bow script modified
    bow rotates according to mouse.
    camera projection modified

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
-
     public float offset;
     public Transform shotPoint;
     public GameObject projectile;
@@ -12,24 +11,35 @@ public class Bow : MonoBehaviour
     public float timeBtwShots;
     public float startTimeBtwShots;
 
-    /*PlayerMovement playerMovement; // Refer to script
-    public GameObject player;
-    public SpriteRenderer spriteRenderer;
-    public float speed;*/
-    // Update is called once per frame
     void Update()
     {
-
-        Vector3 bowPosition = transform.position;
-        //Vector3 mousePosition = Camera.main.ViewportToScreenPoint(Input.GetKey(KeyCode.RightArrow));
-       // Vector3 mousePosition = Camera.main.ViewportToScreenPoint(Input.mousePosition); // when we use mouse
-       // Vector3 difference = mousePosition - bowPosition;
-        Vector3 difference = Camera.main.WorldToScreenPoint(Input.mousePosition) - transform.position;
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
         Debug.Log("" + rotZ + offset);
         if (Input.GetMouseButtonDown(0))
-        {/*
+        {
+            Instantiate(projectile, shotPoint.position, transform.rotation);
+        }
+
+    }
+
+    /*PlayerMovement playerMovement; // Refer to script
+    public GameObject player;
+    public SpriteRenderer spriteRenderer;
+    public float speed;*//*
+    // Update is called once per frame
+   
+        //Vector3 bowPosition = transform.position;
+        //Vector3 mousePosition = Camera.main.ViewportToScreenPoint(Input.GetKey(KeyCode.RightArrow));
+        // Vector3 mousePosition = Camera.main.ViewportToScreenPoint(Input.mousePosition); // when we use mouse
+        // Vector3 difference = mousePosition - bowPosition;
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+        Debug.Log("" + rotZ + offset);
+        if (Input.GetMouseButtonDown(0))
+        {*//*
             if (playerMovement.PlayerMovingDirection() == 1)
             {
                 spriteRenderer.flipX = true;
@@ -40,11 +50,11 @@ public class Bow : MonoBehaviour
             {
                 spriteRenderer.flipX = false;
                 transform.Translate(Vector2.right * speed * Time.deltaTime);
-            }*/
+            }*//*
             Instantiate(projectile, shotPoint.position, transform.rotation);
         }
 
-        /*if (timeBtwShots <= 1)
+        *//*if (timeBtwShots <= 1)
         {
             
             timeBtwShots = startTimeBtwShots;
@@ -66,7 +76,8 @@ public class Bow : MonoBehaviour
 
             myTransform.Translate(new Vector2(-1,  0) * arrowSpeed * Time.deltaTime);
 
-        }*/
+        }
+     }*/
 
-    }
+    
 }
