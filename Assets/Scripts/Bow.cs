ad627e4d04ea26430bf18cf9ce762a09266ff261
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-10-08 11:03:17 +0500

    Player ranged attack completed
    Range attack of player is modified

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -20,64 +20,9 @@ public class Bow : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(projectile, shotPoint.position, transform.rotation);
+
         }
 
     }
-
-    /*PlayerMovement playerMovement; // Refer to script
-    public GameObject player;
-    public SpriteRenderer spriteRenderer;
-    public float speed;*//*
-    // Update is called once per frame
-   
-        //Vector3 bowPosition = transform.position;
-        //Vector3 mousePosition = Camera.main.ViewportToScreenPoint(Input.GetKey(KeyCode.RightArrow));
-        // Vector3 mousePosition = Camera.main.ViewportToScreenPoint(Input.mousePosition); // when we use mouse
-        // Vector3 difference = mousePosition - bowPosition;
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
-        Debug.Log("" + rotZ + offset);
-        if (Input.GetMouseButtonDown(0))
-        {*//*
-            if (playerMovement.PlayerMovingDirection() == 1)
-            {
-                spriteRenderer.flipX = true;
-                transform.Translate(Vector2.left * speed * Time.deltaTime);
-            }
-
-            else
-            {
-                spriteRenderer.flipX = false;
-                transform.Translate(Vector2.right * speed * Time.deltaTime);
-            }*//*
-            Instantiate(projectile, shotPoint.position, transform.rotation);
-        }
-
-        *//*if (timeBtwShots <= 1)
-        {
-            
-            timeBtwShots = startTimeBtwShots;
-        }
-        else
-        {
-            timeBtwShots -= Time.deltaTime;
-        }*/
-        
-     /*   if (canRotate == true)
-        {
-
-            myTransform.Rotate(0, (mousePoint.y / 10) * Time.deltaTime * rotationSpeed, 0);
-
-        }
-
-        if (canTranslate == true)
-        {
-
-            myTransform.Translate(new Vector2(-1,  0) * arrowSpeed * Time.deltaTime);
-
-        }
-     }*/
-
     
 }
--- Assets/Scripts/ProjectileShooting.cs
+++ Assets/Scripts/ProjectileShooting.cs
@@ -10,53 +10,49 @@ public class ProjectileShooting : MonoBehaviour
     public LayerMask enemy;
     public Transform arrowTransform;
     PlayerMovement playerMovement; // Refer to script
-    public GameObject player;
     public SpriteRenderer spriteRenderer;
+    Vector2 pos;
+    Vector2 velocity;
+    void Awake()
+    {
+        playerMovement = GameObject.Find("Player_Goblin").GetComponent<PlayerMovement>();
+        velocity = new Vector3(speed * Time.deltaTime, 0, 0);
+        pos = transform.position;
+    }
     void Start()
     {
         Invoke("DestroyProjectile", lifeTime);
-        playerMovement = GameObject.Find("Player_Goblin").GetComponent<PlayerMovement>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-        if(playerMovement.PlayerMovingDirection() == 1)
+        if (playerMovement.PlayerMovingDirection() == 1)
         {
             spriteRenderer.flipX = true;
-            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            velocity = (Vector3.left * speed * Time.deltaTime);
         }
-           
+
         else
         {
             spriteRenderer.flipX = false;
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            velocity = (Vector3.right * speed * Time.deltaTime);
         }
-          
+    }
+    void Update()
+    {
+        transform.Translate(velocity);
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, enemy);
-        if(hitInfo.collider != null)
+         
+        if (hitInfo.collider != null)
         {
-            if(hitInfo.collider.CompareTag("Skeleton"))
+            if (hitInfo.collider.CompareTag("Skeleton"))
             {
                 Debug.Log("Arrow hit Skeleton");
                 hitInfo.collider.GetComponent<SkeletonEnemyMovement>().TakeDemage(40);
             }
             DestroyProjectile();
         }
-        //arrowTransform.rotation = Quaternion.Euler(0f, 0f, 180);
-      
-        /*if (Input.GetKey(KeyCode.LeftArrow))
-        {*//*
-            arrowTransform.rotation = Quaternion.Euler(0f, 0f, 180);*//*
-            transform.Translate(Vector2.left * speed * Time.deltaTime);
-        }
-        else*/
-            
     }
     void DestroyProjectile()
     {
-       // Instantiate(destroyEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
+    
 }
