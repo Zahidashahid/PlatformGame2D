392a0dc948e7fc49fad52b4866756a789dccedc0
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-10-14 11:21:51 +0500

    Main Menu Sounds

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,25 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource onClickBtnSound;
     public void PlayGame()
     {
+        OnBtnClickSound();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void Level1()
     {
+        OnBtnClickSound();
         SceneManager.LoadScene("Level 1");
     }
     public void Level2()
     {
+        OnBtnClickSound();
         SceneManager.LoadScene("Level 2");
     }
     public void Level3()
     {
+        OnBtnClickSound();
         SceneManager.LoadScene("Level 3");
     }
     public void QuitGame()
     {
+        OnBtnClickSound();
         Debug.Log("Application Quit");
         Application.Quit();
+    }
+    public void OnBtnClickSound()
+    {
+        onClickBtnSound.Play();
     }
 }
