c8d839d37b5e04db42e92cc1f45c1f7eb3d7de77
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2022-01-17 12:08:55 +0500

    Graphics setting added

--- Assets/Scripts/SettingScripts/GraphicSetting.cs
+++ Assets/Scripts/SettingScripts/GraphicSetting.cs
@@ -1,16 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GraphicSetting : MonoBehaviour
 {
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+    private void Start()
+    {
+       resolutions =  Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+            
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     public void SetGraphic(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
-    public void SetResoultion()
+    public void SetResoultion(int resolutionIndex)
     {
-        
+        Resolution resolution = resolutions[resolutionIndex];
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 
     
